b1a1de23d6a2939b70f6961c2d6792c18861a04d
janaki <janakidd.iyengar@gmail.com>
2022-01-20 20:16:14 -0500

    second commit

---
+++ src/test/java/com/employeeapi/utilities/RestUtils.java
@@ -0,0 +1,44 @@
+package com.employeeapi.utilities;
+
+import org.apache.commons.lang3.RandomStringUtils;
+
+  public class RestUtils {
+
+	
+	public static String empName() {
+		String generatedString = RandomStringUtils.randomAlphabetic(1);
+		return ("John"+generatedString);
+	}
+
+	public static String empSal() {
+		String generatedString = RandomStringUtils.randomNumeric(5);
+		return (generatedString);
+	}
+	
+	public static String empAge() {
+		String generatedString = RandomStringUtils.randomNumeric(2);
+		return (generatedString);
+	}
+  }
+	/*public class RestUtils {
+
+		
+		public static String pmgName() {
+			String generatedString = RandomStringUtils.randomAlphabetic(1);
+			return (generatedString);
+		}
+
+		public static String pmgdes() {
+			String generatedString = RandomStringUtils.randomAlphabetic(1);
+			return (generatedString);
+		}
+		
+		public static String pmgonl() {
+			String generatedString = RandomStringUtils.randomAlphabetic(1);
+			return (generatedString);
+		}
+	//	public static String pmgid() {
+		//	String generatedString = RandomStringUtils.randomNumeric(5);
+	//		return (generatedString);
+
+	}*/
